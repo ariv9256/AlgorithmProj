596c17176f0502f87363477e0a26d908c9b059d6
Angela Rivas <snowcats001@gmail.com>
2018-10-23 12:27:07 -0600

    Removed unnecessary code and organized content.

---
+++ AlgorithmProj/AlgorithmController/AlgorithmViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  AlgorithmProj
+//
+//  Created by Rivas, Angela on 10/23/18.
+//  Copyright © 2018 CTEC CSP. All rights reserved.
+//
+
+import UIKit
+
+class AlgorithmViewController: UIViewController
+{
+    
+
+
+}
+
